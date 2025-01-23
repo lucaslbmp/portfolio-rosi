783bc20df1a6001206805a80d4efcb66530ba62d
Lucas <lucaslbmp@github.com>
2025-01-23 20:35:04 -0300

    Adding capitalize function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
 export function compareSearch(search: string, target: string) {
   return !!search.length && target.startsWith(search);
 }
+
+
+export function capitalize(word: string) {
+  return word.at(0)?.toUpperCase() + word.substring(1);
+}
