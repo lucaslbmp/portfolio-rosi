1588b54456250f04276098d21b41ea7f69ec7464
Lucas <lucaslbmp@github.com>
2025-01-24 14:15:18 -0300

    Changing search bar bottom margin

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -6,7 +6,7 @@ const SearchBar = ({ className, onSearch }: SearchBarProps) => {
     <InputField
       label=""
       className={
-        "relative mb-12 mx-auto after:pl-4 after:content-['🔎︎'] after:absolute after:left-0 after:top-[25%] [&_input]:pl-10 [&_input]:focus-within:outline-[unset]" +
+        "relative mb-8 mx-auto after:pl-4 after:content-['🔎︎'] after:absolute after:left-0 after:top-[25%] [&_input]:pl-10 [&_input]:focus-within:outline-[unset]" +
         " " +
         className
       }
