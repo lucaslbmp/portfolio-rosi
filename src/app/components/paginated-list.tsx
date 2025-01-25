17d7e1aef1fc8ac19d8bfd2248c9431f9f203b78
Lucas <lucaslbmp@github.com>
2025-01-25 15:58:22 -0300

    Creating PaginatedList component

---
+++ src/app/components/paginated-list.tsx
@@ -0,0 +1,65 @@
+import { paginate } from "@/utils";
+import Pagination from "rc-pagination";
+import { JSX, useEffect, useState } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+
+type ArrowType = "page" | "prev" | "next" | "jump-prev" | "jump-next";
+
+interface PaginatedListProps<T> {
+  list: Array<T>;
+  pageSize: number;
+  mapFunction: (value: T, index: number, array: T[]) => JSX.Element;
+  listClassName?: string;
+}
+
+const prevNextArrow = (
+  current: number,
+  type: ArrowType,
+  originalElement: React.ReactNode
+) => (
+  <button>
+    {type === "prev" ? (
+      <FaAngleLeft />
+    ) : type === "next" ? (
+      <FaAngleRight />
+    ) : (
+      originalElement
+    )}
+  </button>
+);
+
+const PaginatedList = <T,>({
+  list,
+  pageSize,
+  mapFunction,
+  listClassName,
+}: PaginatedListProps<T>) => {
+  const [currPage, setCurrPage] = useState(1);
+
+  const updatePage = (page: number) => {
+    setCurrPage(page);
+  };
+
+  useEffect(() => {
+    setCurrPage(1);
+  }, [list]);
+
+  return (
+    <>
+      <div className={listClassName}>
+        {paginate<T>(list, pageSize, currPage).map(mapFunction)}
+      </div>
+      <Pagination
+        className="flex gap-4 justify-center [&_li]:flex [&_button]:justify-center [&_button]:flex [&_button]:items-center [&_button]:border-2 [&_button]:w-8 [&_button]:h-8 [&_button]:rounded-md hover:[&_button]:border-primary [&_li[class$=active]_button]:bg-primaryLight [&_li[class$=active]_button]:text-foregroundSecondary"
+        pageSize={pageSize}
+        onChange={updatePage}
+        current={currPage}
+        total={list.length}
+        showSizeChanger={true}
+        itemRender={prevNextArrow}
+      />
+    </>
+  );
+};
+
+export default PaginatedList;
--- src/app/components/products-list.tsx
+++ src/app/components/products-list.tsx
@@ -3,49 +3,22 @@
 import ProductCard from "@/components/product-card";
 import SearchBar from "@/components/search-bar";
 import { categories, products } from "@/data";
-import { ProductCardProps } from "@/types";
-import { compareSearch, paginate as paginate } from "@/utils";
-import Pagination from "rc-pagination";
+import { compareSearch } from "@/utils";
 import { useEffect, useState } from "react";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import CategoriesButtons from "./categories-buttons";
-
-type ArrowType = "page" | "prev" | "next" | "jump-prev" | "jump-next";
+import PaginatedList from "./paginated-list";
 
 const ProductsList = () => {
   const allCategories = "Todas as categorias";
   const pageSize = 8;
   const [search, setSearch] = useState<string>();
   const [filteredProducts, setFilteredProducts] = useState(products);
-  const [currPage, setCurrPage] = useState(1);
   const [currentCategory, setCurrentCategory] = useState<string>(allCategories);
   const [categoriesList, setCategoriesList] = useState<string[]>();
-  // const { register, getValues } = useForm<CategoriesForm>();
-
-  const updatePage = (page: number) => {
-    setCurrPage(page);
-  };
 
   const handleCategoryChange = (value: string) => {
     setCurrentCategory(value);
-    setCurrPage(1);
   };
-
-  const prevNextArrow = (
-    current: number,
-    type: ArrowType,
-    originalElement: React.ReactNode
-  ) => (
-    <button>
-      {type === "prev" ? (
-        <FaAngleLeft />
-      ) : type === "next" ? (
-        <FaAngleRight />
-      ) : (
-        originalElement
-      )}
-    </button>
-  );
 
   useEffect(() => {
     const _categories = [allCategories, ...categories];
@@ -60,7 +33,6 @@ const ProductsList = () => {
           currentCategory === p.category || currentCategory === allCategories
       )
       .filter((p) => !search || compareSearch(search, p.name));
-    console.log(filtered);
     setFilteredProducts(filtered);
   }, [currentCategory, search]);
 
@@ -75,21 +47,11 @@ const ProductsList = () => {
         defaultCategory={allCategories}
         handleCategoryChange={handleCategoryChange}
       />
-      <div className="grid grid-cols-auto-fill-250 gap-4">
-        {paginate<ProductCardProps>(filteredProducts, pageSize, currPage).map(
-          (p) => (
-            <ProductCard key={p.name} {...p} />
-          )
-        )}
-      </div>
-      <Pagination
-        className="flex gap-4 justify-center [&_li]:flex [&_button]:justify-center [&_button]:flex [&_button]:items-center [&_button]:border-2 [&_button]:w-8 [&_button]:h-8 [&_button]:rounded-md hover:[&_button]:border-primary [&_li[class$=active]_button]:bg-primaryLight [&_li[class$=active]_button]:text-foregroundSecondary"
+      <PaginatedList
+        list={filteredProducts}
         pageSize={pageSize}
-        onChange={updatePage}
-        current={currPage}
-        total={filteredProducts.length}
-        showSizeChanger={true}
-        itemRender={prevNextArrow}
+        mapFunction={(p) => <ProductCard key={p.name} {...p} />}
+        listClassName="grid grid-cols-auto-fill-250 gap-4"
       />
     </section>
   );
