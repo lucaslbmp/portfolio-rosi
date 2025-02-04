7290f6d231a4ff7d4b8f999bded2ccff21e0a429
Lucas <lucaslbmp@github.com>
2025-02-04 01:41:17 -0300

    Increasing request body size limit to 5MB

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rosi-portfolio-pictures.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
