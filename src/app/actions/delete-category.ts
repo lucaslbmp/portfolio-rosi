82c8d5a5311d2a779a1aaa3a26622f979602bb0a
Lucas <lucaslbmp@github.com>
2025-02-13 17:29:21 -0300

    Fix: deleting S3 images when deleting category

--- src/app/actions/delete-category.ts
+++ src/app/actions/delete-category.ts
@@ -2,15 +2,24 @@
 
 import prisma from "@/lib/prisma";
 import { toNumber } from "@/utils";
+import { deleteImageFromS3 } from "@/utils/delete-image";
 import { revalidatePath } from "next/cache";
 
 export async function deleteCategoryAction(formData: FormData) {
   try {
     const id = toNumber(formData.get("categoryId") as string);
 
-    const category = await prisma.category.delete({
-      where: { id },
-    });
+    const images = (
+      await prisma.product.findMany({ where: { categoryId: id } })
+    ).map(({ image }) => image);
+    const [, category] = await prisma.$transaction([
+      prisma.product.deleteMany({ where: { id } }),
+      prisma.category.delete({
+        where: { id },
+      }),
+    ]);
+
+    images.forEach((image) => deleteImageFromS3(image));
 
     console.log("Deletou categoria com sucesso: " + category);
     revalidatePath("/");
