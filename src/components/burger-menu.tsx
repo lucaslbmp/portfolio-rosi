cfce2728b5bc9fe901d1e502898278ed7834dd69
Lucas <lucaslbmp@github.com>
2025-02-16 14:57:44 -0300

    Adding burger menu component to header navbar

---
+++ src/components/burger-menu.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function BurgerMenu({
+  menuItems,
+  className,
+}: {
+  menuItems: { name: string; href: string }[];
+  className?: string;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className={twMerge("relative", className)}>
+      {/* Hamburger Button */}
+      <button
+        className="text-2xl p-2 w-[37px]"
+        onClick={() => setIsOpen((prev) => !prev)}
+        aria-label="Toggle menu"
+      >
+        <i className={`fas ${isOpen ? "fa-times" : "fa-bars"}`}></i>
+      </button>
+
+      {/* Menu */}
+      <ul
+        className={`absolute top-full right-0 bg-white shadow-lg rounded-lg min-w-max transform transition-opacity duration-[500ms] ${
+          isOpen ? "opacity-90" : " opacity-0"
+        } drop-shadow-[0_0_2px_#00000033]`}
+      >
+        {menuItems.map((item, index) => (
+          <li key={index} className="border-b last:border-none px-3">
+            <a
+              href={item.href}
+              className={`block px-4 py-3 text-gray-700 hover:bg-gray-100 transition-colors`}
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,36 @@
 import Link, { LinkProps } from "next/link";
 import React from "react";
+import BurgerMenu from "./burger-menu";
 
 interface NavbarButtonProps extends LinkProps {
   children: React.ReactNode;
 }
 
 const NavbarButton = (props: NavbarButtonProps) => (
-  <Link className="p-3 hover:text-primary" {...props}>
+  <Link className="py-3 px-[1vw] hover:text-primary" {...props}>
     {props.children}
   </Link>
 );
 
 const Navbar = () => {
   return (
-    <nav className="bg-inherit font-opensans text-xl ">
-      <NavbarButton href="#home">Início</NavbarButton>
-      <NavbarButton href="#portfolio">Portfolio</NavbarButton>
-      <NavbarButton href="#about">Sobre</NavbarButton>
-      <NavbarButton href="#contacts">Contatos</NavbarButton>
-    </nav>
+    <>
+      <nav className="bg-inherit font-opensans text-lg hidden md:block">
+        <NavbarButton href="#home">Início</NavbarButton>
+        <NavbarButton href="#portfolio">Portfolio</NavbarButton>
+        <NavbarButton href="#about">Sobre</NavbarButton>
+        <NavbarButton href="#contacts">Contatos</NavbarButton>
+      </nav>
+      <BurgerMenu
+        className="md:hidden"
+        menuItems={[
+          { name: "Início", href: "#home" },
+          { name: "Portfolio", href: "#portfolio" },
+          { name: "Sobre", href: "#about" },
+          { name: "Contatos", href: "#contacts" },
+        ]}
+      />
+    </>
   );
 };
 
