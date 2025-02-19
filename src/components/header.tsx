f52a811abe59f36a01e0107eb869ea1ddd5f466f
Lucas <lucaslbmp@github.com>
2025-02-19 13:08:36 -0300

    Fix: adding home id to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,10 @@ import HeaderBody from "./header-body";
 
 const Header = ({ contacts }: { contacts: ContactItemProps[] }) => {
   return (
-    <header className="flex flex-col bg-background max-w-full sticky top-0 z-10">
+    <header
+      id="home"
+      className="flex flex-col bg-background max-w-full sticky top-0 z-10"
+    >
       <section className="hidden md:flex gap-8 justify-center bg-primaryLight m-0 md:px-16 text-sm text-foregroundSecondary">
         {contacts.map(({ name, icon, content }) => (
           <div key={name} className="flex gap-2 items-center">
