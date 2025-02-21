8fb71236532e8434e9102b33e64e2478286357a4
Lucas <lucaslbmp@github.com>
2025-02-21 14:40:12 -0300

    Changing prisma client to singleton

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Ensures Prisma reads the runtime env variable
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
