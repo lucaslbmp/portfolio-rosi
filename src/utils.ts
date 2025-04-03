151059068674383315eed2edbeb35ba6efa6c01c
Lucas <lucaslbmp@github.com>
2025-04-03 18:44:08 -0300

    Modifying search behavior from starting letter search to substring search

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export function compareSearch(search: string, target: string) {
-  return !!search.length && target.startsWith(search);
+  return !!search.length && target.toLowerCase().includes(search.toLowerCase());
 }
 
 export function paginate<T>(
